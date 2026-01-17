dfd8627bbd29197a9281a2bd650ef1cc7b372a2f
Abraham Pech <abrahamayil415@gmail.com>
2026-01-17 10:39:37 -0600

    To-do list was created

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+//Variables for modal
 const modal = document.getElementById("modal");
 const addButton = document.querySelectorAll(".btn-add");
 const closeModal = document.getElementById("close-modal");
+//Form
+const formExercise = document.getElementById("form-exercise");
+let currentListExercise = null;
 
 addButton.forEach((btn) => {
   btn.addEventListener("click", () => {
+    const cardRoutine = btn.closest(".card-routine");
+    currentListExercise = cardRoutine.querySelector(".list-exercises");
+
     modal.style.display = "flex";
   });
 });
@@ -15,5 +22,43 @@ closeModal.addEventListener("click", () => {
 window.addEventListener("click", (e) => {
   if (e.target === modal) {
     modal.style.display = "none";
+  }
+});
+
+formExercise.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const dataForm = new FormData(e.target);
+
+  //Capitalize the first letter in the name of exercise
+  const exerciseNameRaw = dataForm.get("exercise").trim();
+  const exerciseName =
+    exerciseNameRaw.charAt(0).toUpperCase() + exerciseNameRaw.slice(1);
+
+  const dataExercises = {
+    name: exerciseName,
+    weight: dataForm.get("weight"),
+    sets: dataForm.get("sets"),
+    reps: dataForm.get("reps"),
+  };
+
+  //Variables to add the new exercise
+  const divExercises = document.querySelectorAll("list-exercises");
+
+  const divNewExercise = document.createElement("div");
+  divNewExercise.classList.add("exercise");
+
+  const exerciseInfo = document.createElement("p");
+  exerciseInfo.textContent = `${dataExercises.name} ${dataExercises.weight} kg ${dataExercises.sets} x ${dataExercises.reps}`;
+
+  divNewExercise.appendChild(exerciseInfo);
+
+  if (currentListExercise) {
+    currentListExercise.appendChild(divNewExercise);
+
+    e.target.reset();
+    modal.style.display = "none";
+  } else {
+    alert("The list of current exercises could not be found");
   }
 });
